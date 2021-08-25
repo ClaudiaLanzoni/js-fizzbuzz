55d9b5c786a5be51cbe193cb324cfa8df62697dd
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-08-25 19:29:37 +0200

    exercise finalized

--- myscript.js
+++ myscript.js
@@ -1,15 +1,4 @@
-/*Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi "Buzz". Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
-Prima di partire a scrivere codice poniamoci qualche domanda:
-Come faccio a sapere se un numero è divisibile per un altro?
-Abbiamo visto qualcosa di particolare che potremmo usare?
-Consigli del giorno:
-Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"*/
-
-
-
-
-for (let i = 1; i <= 100; i++) {
+/*for (let i = 1; i <= 100; i++) {
     
     let numeri = [i];
     let multiploDiTre = (i % 3 == 0);
@@ -20,9 +9,27 @@ for (let i = 1; i <= 100; i++) {
         numeri.splice (multiploDiTre, 1, "Fizz");
         
     } else if (i % 5 == 0) {
-        numeri.splice (multiploDiCinque, 1, "Bizz");
-    } 
+        numeri.splice (multiploDiCinque, 1, "Buzz");
+    } else if (i % )
     
     console.log(numeri)
+}*/
+
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");   
+
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+        
+    } else if (i % 5 == 0) {
+        console.log("Buzz"); 
+
+    }  else {
+        console.log(i)
+    }
+    
 }
 
+//perchè se si mette && come ultimo else if non funziona e invece messo per primo sì?
