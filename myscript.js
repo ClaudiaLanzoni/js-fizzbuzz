066accd4272482bf62d160d57fe8c0aa7322fee9
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-08-25 19:43:55 +0200

    first solution (wrong) completed

--- myscript.js
+++ myscript.js
@@ -3,18 +3,27 @@
     let numeri = [i];
     let multiploDiTre = (i % 3 == 0);
     let multiploDiCinque = (i % 5 == 0);
+    let multipliDiTreECinque = (i % 3 == 0 && i % 5 == 0);
 
 
-    if (i % 3 == 0) {
-        numeri.splice (multiploDiTre, 1, "Fizz");
+    if (i % 3 == 0 && i % 5 == 0) {
+        numeri.splice (multipliDiTreECinque, 1, "FizzBuzz");
         
     } else if (i % 5 == 0) {
         numeri.splice (multiploDiCinque, 1, "Buzz");
-    } else if (i % )
-    
-    console.log(numeri)
-}*/
 
+    } else if (i % 3 == 0) {
+        numeri.splice (multiploDiTre, 1, "Fizz");
+    
+    } 
+    
+    console.log(numeri);
+}
+*/
+
+
+
+//perchè se si mette && come ultimo else if non funziona e invece messo per primo sì?
 for (let i = 1; i <= 100; i++) {
 
     if (i % 3 == 0 && i % 5 == 0) {
@@ -31,5 +40,3 @@ for (let i = 1; i <= 100; i++) {
     }
     
 }
-
-//perchè se si mette && come ultimo else if non funziona e invece messo per primo sì?
